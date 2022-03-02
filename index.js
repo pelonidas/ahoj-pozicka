23c4666122233957c295f3178ba492938f8ba0c4
pelonidas <janx803y@easv365.dk>
2022-03-02 19:59:14 +0100

    Add basic comments

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+//Setting view engine to .ejs
 app.set('view engine', 'ejs');
+// Defying a views path
 app.set('views', path.join(__dirname, 'views'));
 
+//Middleware for retrieving POST forms
 app.use(express.urlencoded({extended: true}))
 
+
+// Home route
 app.get('/', (req, res) => {
     res.render('home')
 });
 
+
+// Listening to a port
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
