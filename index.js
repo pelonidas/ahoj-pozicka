10d7d440ed8f961be51f28f271542ed17af4f0be
pelonidas <janx803y@easv365.dk>
2022-03-18 02:21:19 +0100

    simple form send mail

--- index.js
+++ index.js
@@ -215,6 +215,59 @@ app.post('/form/step-1', async (req, res) => {
     res.redirect('/form/step-2')
 })
 
+app.post('/form', async (req, res) => {
+    const data = req.body
+    let output = ""
+    if (data.interest) {
+        output = `
+            <h3>Contact simple form</h3>
+            <ul>
+                  <li>Meno a priezvisko <b>${data.name}</b></li>
+                  <li>Telefonne cislo: <b>${data.phoneNumber}</b></li>
+                  <li>Email: <b>${data.email}</b></li>
+                  <li>Sprava: <b>${data.message}</b></li>
+                  <li>Mam zaujem o: ${data.interest}</li>
+            </ul>
+        `;
+    } else {
+        output = `
+        <h3>Contact simple form</h3>
+            <ul>
+                  <li>Meno a priezvisko <b>${data.name}</b></li>
+                  <li>Telefonne cislo: <b>${data.phoneNumber}</b></li>
+                  <li>Email: <b>${data.email}</b></li>
+                  <li>Sprava: <b>${data.message}</b></li>
+            </ul>
+        `
+    }
+
+    let transporter = nodemailer.createTransport({
+        host: "mail.websupport.sk",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'test01@mail.dpmarketing.sk', // generated ethereal user
+            pass: 'Webtestemail123!', // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Test Person" <test01@mail.dpmarketing.sk>', // sender address
+        to: `test01@mail.dpmarketing.sk, `, // list of receivers
+        subject: "Udaje", // Subject line
+        text: "Hello world?", // plain text body
+        html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+    res.redirect('/')
+})
+
 
 // Handling undefined routes
 // app.all('*', (req, res, next) => {
