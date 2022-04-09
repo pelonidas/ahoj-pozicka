733f68c06c3b315addccc824e1ed9865605e2f01
pelonidas <janx803y@easv365.dk>
2022-04-10 00:40:54 +0200

    test centrala swiper.js implemented

--- public/javascripts/swiper.js
+++ public/javascripts/swiper.js
@@ -2,11 +2,13 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 2,
+    spaceBetween: 50,
 
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
 
     // If we need pagination
     pagination: {
