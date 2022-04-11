a646f8d6737624462ab5c7e35b7bcf2f6cf29081
pelonidas <janx803y@easv365.dk>
2022-04-11 03:33:35 +0200

    centrala.ejs update

--- public/javascripts/swiper.js
+++ public/javascripts/swiper.js
@@ -3,7 +3,30 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 2,
-    spaceBetween: 50,
+    spaceBetween: 40,
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+const swiperMobile = new Swiper('.swiper-mobile', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    // spaceBetween: 40,
 
     // autoplay: {
     //     delay: 2500,
