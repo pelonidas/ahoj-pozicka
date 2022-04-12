6404dd3ec347607f04badcb099e67aea69cd7996
pelonidas <janx803y@easv365.dk>
2022-04-12 23:50:57 +0200

    no results translated

--- public/javascripts/customDropdown.js
+++ public/javascripts/customDropdown.js
@@ -1,4 +1,12 @@
-$('.ui.dropdown').dropdown()
+$('.ui.dropdown').dropdown({
+    message: {
+        noResults: 'Neboli nájdené žiadne výsledky.'
+    }
+})
+// $.fn.dropdown.settings.message = function (search) {
+//     console.log(search)
+// }
+
 let test = document.querySelector('.ui')
 test.addEventListener('click', () => {
     test.classList.remove('upward')
