3305b06b80fff599a735f428e2d32e060dbc90ee
pelonidas <janx803y@easv365.dk>
2022-05-02 13:31:13 +0200

    calc update

---
+++ public/javascripts/RefinancovanieCalculator.js
@@ -0,0 +1,103 @@
+// Refinancovanie inputs
+let moneySlideRef = document.querySelector('#money_range_ref'),
+    yearSlideRef = document.querySelector('#year_range_ref'),
+    moneyFieldRef = document.querySelector('#money_input_ref'),
+    yearFieldRef = document.querySelector('#year_input_ref')
+
+let mesSumRef = document.getElementById("mes_sum_ref");
+
+let refData = '';
+
+if (moneySlideRef) {
+    const calcMonthlyCostRef = function (moneyVal, yearVal) {
+        let money = Number(moneyVal);
+        let years = Number(yearVal);
+
+        let moneyMax = Number(moneySlideRef.max);
+        let moneyMin = Number(moneySlideRef.min);
+        let yearMax = Number(yearSlideRef.max);
+        let yearMin = Number(yearSlideRef.min);
+
+        if (isNaN(money) || isNaN(years)) {
+            return "";
+        }
+
+        if (money > moneyMax || money < moneyMin) {
+            return "";
+        }
+
+        if (years > yearMax || years < yearMin) {
+            return "";
+        }
+
+        let monthlyCost = money * ((0.119 / 12) / (1 - Math.pow((1 / (1 + (0.119 / 12))), years * 12)));
+        monthlyCost = monthlyCost.toFixed(2);
+
+        data = `${monthlyCost}`;
+        interest.value = data;
+
+        return monthlyCost + " €";
+    }
+
+    function moneySlideHandlerRef(e) {
+        moneyFieldRef.value = numeral(e.target.value).format('00,000 €')
+
+        doCalcRef();
+    }
+
+    function yearSlideHandlerRef(e) {
+        yearFieldRef.value = numeral(e.target.value).format('(0 r)');
+
+        doCalcRef();
+    }
+
+    function moneyInputHandlerRef(e) {
+        let moneyValue = e.target.value;
+
+        if (moneyValue) {
+            if (!isNaN(moneyValue)) {
+                moneyValue = Number(moneyValue);
+                moneyValue = Math.ceil(moneyValue/100)*100;
+                moneySlideRef.value = moneyValue;
+                moneySlideRef.style.setProperty('--value', String(moneyValue));
+                doCalcRef();
+            }
+        }
+    }
+
+    function yearInputHandlerRef(e) {
+        let val = e.target.value;
+
+        if (val) {
+            if (!isNaN(val)) {
+                val = Number(val);
+                val = Math.ceil(val);
+                yearSlideRef.value = val;
+                yearSlideRef.style.setProperty('--value', String(val));
+                doCalcRef();
+            }
+        }
+    }
+
+    function doCalcRef(){
+        let moneyVal = moneySlideRef.value;
+        let yearVal = yearSlideRef.value;
+        // test = new AutoNumeric(money_field)
+
+        mesSumRef.innerHTML = calcMonthlyCostRef(moneyVal, yearVal);
+    }
+
+    moneyFieldRef.addEventListener('focusout', (e) => {
+        moneyFieldRef.value = numeral(e.target.value).format('00,000.00 €')
+    })
+
+    yearFieldRef.addEventListener('focusout', (e) => {
+        yearSlideRef.value = numeral(e.target.value).format('(0 r)');
+    })
+
+    moneyFieldRef.addEventListener('input', moneyInputHandlerRef);
+    yearFieldRef.addEventListener('input', yearInputHandlerRef);
+
+    moneySlideRef.addEventListener('input', moneySlideHandlerRef);
+    yearSlideRef.addEventListener('input', yearSlideHandlerRef);
+}
--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -1,5 +1,4 @@
 //data
-const AutoNumeric = window.autonumeric;
 let numeral = window.numeral
 
 numeral.register('format', 'euro', {
@@ -84,96 +83,107 @@ var mes_sum = document.getElementById("mes_sum");
 
 let test;
 
-const calcMonthlyCost = function (moneyVal, yearVal) {
-    let money = Number(moneyVal);
-    let years = Number(yearVal);
+if (money_field) {
+    const calcMonthlyCost = function (moneyVal, yearVal) {
+        let money = Number(moneyVal);
+        let years = Number(yearVal);
 
-    let moneyMax = Number(money_slide.max);
-    let moneyMin = Number(money_slide.min);
-    let yearMax = Number(year_slide.max);
-    let yearMin = Number(year_slide.min);
+        let moneyMax = Number(money_slide.max);
+        let moneyMin = Number(money_slide.min);
+        let yearMax = Number(year_slide.max);
+        let yearMin = Number(year_slide.min);
 
-    if (isNaN(money) || isNaN(years)) {
-        return "";
+        if (isNaN(money) || isNaN(years)) {
+            return "";
+        }
+
+        if (money > moneyMax || money < moneyMin) {
+            return "";
+        }
+
+        if (years > yearMax || years < yearMin) {
+            return "";
+        }
+
+        let monthlyCost;
+
+        if (years === 1) {
+            monthlyCost = money * ((0.3632 / 12) / (1 - Math.pow((1 / (1 + (0.3632 / 12))), years * 12)));
+        }
+        if (years >= 2 && years <= 5) {
+            monthlyCost = money * ((0.1794 / 12) / (1 - Math.pow((1 / (1 + (0.1794 / 12))), years * 12)));
+        }
+        if (years > 5 && years <= 8) {
+            monthlyCost = money * ((0.1656 / 12) / (1 - Math.pow((1 / (1 + (0.1656 / 12))), years * 12)));
+        }
+
+        monthlyCost = monthlyCost.toFixed(2);
+
+        data = `${monthlyCost}`;
+        interest.value = data;
+
+        return monthlyCost + " €";
     }
 
-    if (money > moneyMax || money < moneyMin) {
-        return "";
+    function moneySlideHandler(e) {
+        money_field.value = numeral(e.target.value).format('00,000 €')
+
+        doCalc();
     }
 
-    if (years > yearMax || years < yearMin) {
-        return "";
+    function yearSlideHandler(e) {
+        year_field.value = numeral(e.target.value).format('(0 r)');
+
+        doCalc();
     }
 
-    let monthlyCost = money * ((0.1656 / 12) / (1 - Math.pow((1 / (1 + (0.1656 / 12))), years * 12)));
-    monthlyCost = monthlyCost.toFixed(2);
+    function moneyInputHandler(e) {
+        let moneyValue = e.target.value;
 
-    data = `${monthlyCost}`;
-    interest.value = data;
-
-    return monthlyCost + " €";
-}
-
-
-
-function moneySlideHandler(e) {
-    money_field.value = numeral(e.target.value).format('00,000 €')
-
-    doCalc();
-}
-
-function yearSlideHandler(e) {
-    year_field.value = numeral(e.target.value).format('(0 r)');
-
-    doCalc();
-}
-
-function moneyInputHandler(e) {
-    let moneyValue = e.target.value;
-
-    if (moneyValue) {
-        if (!isNaN(moneyValue)) {
-            moneyValue = Number(moneyValue);
-            moneyValue = Math.ceil(moneyValue/100)*100;
-            money_slide.value = moneyValue;
-            money_slide.style.setProperty('--value', String(moneyValue));
-            doCalc();
+        if (moneyValue) {
+            if (!isNaN(moneyValue)) {
+                moneyValue = Number(moneyValue);
+                moneyValue = Math.ceil(moneyValue/100)*100;
+                money_slide.value = moneyValue;
+                money_slide.style.setProperty('--value', String(moneyValue));
+                doCalc();
+            }
         }
     }
-}
 
-function yearInputHandler(e) {
-    let val = e.target.value;
+    function yearInputHandler(e) {
+        let val = e.target.value;
 
-    if (val) {
-        if (!isNaN(val)) {
-            val = Number(val);
-            val = Math.ceil(val);
-            year_slide.value = val;
-            year_slide.style.setProperty('--value', String(val));
-            doCalc();
+        if (val) {
+            if (!isNaN(val)) {
+                val = Number(val);
+                val = Math.ceil(val);
+                year_slide.value = val;
+                year_slide.style.setProperty('--value', String(val));
+                doCalc();
+            }
         }
     }
+
+    function doCalc(){
+        let moneyVal = money_slide.value;
+        let yearVal = year_slide.value;
+        // test = new AutoNumeric(money_field)
+
+        mes_sum.innerHTML = calcMonthlyCost(moneyVal, yearVal);
+    }
+
+    money_field.addEventListener('focusout', (e) => {
+        money_field.value = numeral(e.target.value).format('00,000.00 €')
+    })
+
+    year_field.addEventListener('focusout', (e) => {
+        year_field.value = numeral(e.target.value).format('(0 r)');
+    })
+
+    money_field.addEventListener('input', moneyInputHandler);
+    year_field.addEventListener('input', yearInputHandler);
+
+    money_slide.addEventListener('input', moneySlideHandler);
+    year_slide.addEventListener('input', yearSlideHandler);
 }
-
-function doCalc(){
-    let moneyVal = money_slide.value;
-    let yearVal = year_slide.value;
-    // test = new AutoNumeric(money_field)
-
-    mes_sum.innerHTML = calcMonthlyCost(moneyVal, yearVal);
-}
-
-money_field.addEventListener('focusout', (e) => {
-    money_field.value = numeral(e.target.value).format('00,000.00 €')
-})
-
-year_field.addEventListener('focusout', (e) => {
-    year_field.value = numeral(e.target.value).format('(0 r)');
-})
-
-money_field.addEventListener('input', moneyInputHandler);
-year_field.addEventListener('input', yearInputHandler);
-
-money_slide.addEventListener('input', moneySlideHandler);
-year_slide.addEventListener('input', yearSlideHandler);
